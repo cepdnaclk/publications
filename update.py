cf67dee9e93e59c069622fd537c35387a6acd2b4
Gihan Jayatilaka <gihan@umd.edu>
2022-07-24 18:56:07 -0700

    Add files via upload

--- update.py
+++ update.py
@@ -22,51 +22,44 @@ if __name__=="__main__":
 
     outFile = open(args.outputFile, "w")
     outFile.write('''
-    <html>
-        <head>
-            <title>Publications</title>
-            <link rel="stylesheet" href="./styles.css">
-        </head>
-        <body>
-            <h1>Publications from the Department of Computer Engineering, University of Peradeniya</h1>
-            <hr>
+<html>
+    <head>
+        <title>Publications</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+    <body>
+        <h1>Publications from the Department of Computer Engineering, University of Peradeniya</h1>
+        <hr>
 
-            <ul>
-                <li>The source code for this webpage is <a href="https://github.com/cepdnaclk/publications">here</a></li>
-                <li>If a publication is missing, please add it using <a href="https://forms.gle/rT6Bt8JPzoMz4qGM9">this</a> form.</li>
-                <li>This webpage is based on the University of Peradeniya, Department of Computer Engineering <a href="https://api.ce.pdn.ac.lk/">API</a>.</li>
-                <li>If you have any doubts, please reach out to the maintainers listed <a href="https://github.com/orgs/cepdnaclk/teams/admins-cepdnaclk-github-io/members">here</a>. If you are not a member of the Github organization to view that link, please reach out to <a href="https://people.ce.pdn.ac.lk/students/e17/154/">Akila</a>. If everything else fails, reach out to <a href="https://people.ce.pdn.ac.lk/staff/academic/roshan-ragel/">Prof. Roshan Ragel</a>.</li>
-            </ul>
-
-
-            <table>
-
-    ''')
-    
-
-    
+        <ul>
+            <li>The source code for this webpage is <a href="https://github.com/cepdnaclk/publications">here</a></li>
+            <li>If a publication is missing, please add it using <a href="https://forms.gle/rT6Bt8JPzoMz4qGM9">this</a> form.</li>
+            <li>This webpage is based on the University of Peradeniya, Department of Computer Engineering <a href="https://api.ce.pdn.ac.lk/">API</a>.</li>
+            <li>If you have any doubts, please reach out to the maintainers listed <a href="https://github.com/orgs/cepdnaclk/teams/admins-cepdnaclk-github-io/members">here</a>. If you are not a member of the Github organization to view that link, please reach out to <a href="https://people.ce.pdn.ac.lk/students/e17/154/">Akila</a>. If everything else fails, reach out to <a href="https://people.ce.pdn.ac.lk/staff/academic/roshan-ragel/">Prof. Roshan Ragel</a>.</li>
+        </ul>
 
 
-
+        <table>
+''')
 
     for pubIdx in range(len(JSON)):
 
-        outFile.write("<tr>")
-        outFile.write("<td style=\"vertical-align:top\">{}. </td><td>".format(pubIdx+1))
+        outFile.write("\t\t\t\t<tr>\n")
+        outFile.write("\t\t\t\t\t<td style=\"vertical-align:top\">{}. </td><td>".format(pubIdx+1))
 
         outFile.write("<b>{}</b><br>".format(JSON[pubIdx]["title"]))
 
         if len(JSON[pubIdx]["authors"]) != len(JSON[pubIdx]["author_info"]):
-            outFile.write("<font color=\"red\">Error in author info: Length mismatch. Edit the Json file <a href=\"{}\">here</a>.</font><br>".format(JSON[pubIdx]["api_url"].replace("https://api.ce.pdn.ac.lk/publications/","https://github.com/cepdnaclk/api.ce.pdn.ac.lk/tree/main/publications/")+"index.json"))
+            outFile.write("\n\t\t\t\t\t"+"<font color=\"red\">Error in author info: Length mismatch. Edit the Json file <a href=\"{}\">here</a>.</font><br>".format(JSON[pubIdx]["api_url"].replace("https://api.ce.pdn.ac.lk/publications/","https://github.com/cepdnaclk/api.ce.pdn.ac.lk/tree/main/publications/")+"index.json"))
  
-        outFile.write(", ".join(JSON[pubIdx]["authors"]))
+        outFile.write("\n\t\t\t\t\t"+", ".join(JSON[pubIdx]["authors"]))
         outFile.write("<br>")
         
-        outFile.write(", ".join("<a href=\"{}\">{}</a>".format(aa["profile_url"],aa["name"]) for aa in JSON[pubIdx]["author_info"]))
+        outFile.write("\n\t\t\t\t\t"+", ".join("<a href=\"{}\">{}</a>".format(aa["profile_url"],aa["name"]) for aa in JSON[pubIdx]["author_info"]))
         outFile.write("<br>")
 
-        outFile.write("<i>{}</i> ({})<br>".format(JSON[pubIdx]["venue"],JSON[pubIdx]["year"]))
-
+        outFile.write("\n\t\t\t\t\t"+"<i>{}</i> ({})<br>".format(JSON[pubIdx]["venue"],JSON[pubIdx]["year"]))
+        outFile.write("\n\t\t\t\t\t")
         if JSON[pubIdx]["pdf_url"] != "#":
             outFile.write("<a href=\"{}\">PDF</a> | ".format(JSON[pubIdx]["pdf_url"]))
         
@@ -83,8 +76,8 @@ if __name__=="__main__":
         outFile.write("<br><br>")
 
 
-        outFile.write("</td></tr>")
+        outFile.write("</td></tr>\n")
 
 
         print("Completed publication {} of {} total publications".format(pubIdx+1,len(JSON)))
-    outFile.write("</table><br><hr>Last updated: {} (Sri Lanka time)</body></html>".format(datetime.datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")))
+    outFile.write("\t\t\t</table><br><hr>\n\t\t\tLast updated: {} (Sri Lanka time)\n\t</body>\n</html>".format(datetime.datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")))
