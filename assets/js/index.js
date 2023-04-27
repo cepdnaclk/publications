57bed2df2c500c8569ac5a990fc1f8e956b8adcf
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2023-04-27 09:42:22 +0530

    Bookmark updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,30 @@
 function addBookmark(title, url) {
-  if (window.sidebar && window.sidebar.addPanel) { // Firefox
-    window.sidebar.addPanel(title, url, '');
-  } else if (window.external && ('AddFavorite' in window.external)) { // IE
-    window.external.AddFavorite(url, title);
-  } else if (window.opera && window.print) { // Opera
-    var elem = document.createElement('a');
-    elem.setAttribute('href',url);
-    elem.setAttribute('title',title);
-    elem.setAttribute('rel','sidebar');
-    elem.click();
-  } else { // Other browsers (mainly WebKit - Chrome/Safari)
-    alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + '+D to bookmark this page.');
-  }
+// TODO: Implement a better solution 
+
+  // if (window.sidebar && window.sidebar.addPanel) { // Firefox
+  //   window.sidebar.addPanel(title, url, '');
+  // } else if (window.external && ('AddFavorite' in window.external)) { // IE
+  //   window.external.AddFavorite(url, title);
+  // } else if (window.opera && window.print) { // Opera
+  //   var elem = document.createElement('a');
+  //   elem.setAttribute('href',url);
+  //   elem.setAttribute('title',title);
+  //   elem.setAttribute('rel','sidebar');
+  //   elem.click();
+  // } else { // Other browsers (mainly WebKit - Chrome/Safari)
+  //   alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + '+D to bookmark this page.');
+  // }
   window.location.hash = url
+
+  $('#toast-message').html('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + '+D to bookmark this page.')
+  $('#liveToast').show()
+  setTimeout(function() { 
+        $('#liveToast').hide()
+  }, 4000);
+
 }
+
+
+$(document).ready(()=>{
+  $('#liveToast').addClass('showing').hide()
+});
